a11b8aff5d5f620ba0a6b6bd759716b1eac27d7f
Manuel Lugo <mlugom@unal.edu.co>
2022-10-25 13:47:35 -0500

    Modifying JS file to send continuously twist message

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,21 +1,11 @@
-function publishCmdVel(message){
-
+function publishCmdVel(){
+    cmdVel.publish(twist);
 }
 
 function moveForward(){
-    var twist = new ROSLIB.message({
-        linear: {
-            x: 5,
-            y: 0,
-            z: 0
-        },
-        angular: {
-            x: 0,
-            y: 0,
-            z: 0
-        }
-    });
-    publishCmdVel(twist);
+    var x_vel = 5;
+    twist.linear.x = x_vel;
+    publishCmdVel(twist); 
 }
 
 function moveBackward(){
@@ -35,11 +25,35 @@ function moveBackward(){
 }
 
 function turnLeft(){
-
+    var twist = new ROSLIB.message({
+        linear: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        angular: {
+            x: 0,
+            y: 0,
+            z: 0.8
+        }
+    });
+    publishCmdVel(twist);
 }
 
 function turnRight(){
-
+    var twist = new ROSLIB.message({
+        linear: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        angular: {
+            x: 0,
+            y: 0,
+            z: 0.8
+        }
+    });
+    publishCmdVel(twist);
 }
 
 var ros = new ROSLIB.Ros({
@@ -58,3 +72,33 @@ ros.on('error',function(error){
 ros.on('close',function(){
     console.log('Connection terminated.');
 });
+
+// Cmd_vel message published
+var twist = new ROSLIB.Message({
+    linear: {
+        x: 0,
+        y: 0,
+        z: 0
+    },
+    angular: {
+        x: 0,
+        y: 0,
+        z: 0
+    }
+});
+
+var cmdVel = new ROSLIB.Topic({
+    ros: ros,
+    name: '/cmd_vel',
+    messageType: 'geometry_msgs/Twist'
+});
+
+// Buttons
+var forwardButton = document.getElementById('forward');
+var backwardButton = document.getElementById('backward');
+var leftButton = document.getElementById('left');
+var rightButton = document.getElementById('right');
+while(true){
+    
+}
+
